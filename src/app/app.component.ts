748eac000694db78ae532a282f940a5d51c298c5
“17811482” <amit.sharma@ecentric.ngn.bt>
2023-05-23 02:37:24 +0600

    adsfaf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 
 import { Component } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { NavigationEnd, NavigationExtras, Router } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { ImageService } from './shared/image.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,7 @@ export class AppComponent {
   checkUrl:boolean=true;
   checkAuth:boolean = true;
   receivedData
-  constructor(private router: Router, private service:ImageService){
+  constructor(private router: Router, private service:ImageService,private location: Location){
   
   }
   ngOnInit(): void {
@@ -53,5 +54,132 @@ export class AppComponent {
     });
     //end
   
+  }
+  //dryGrocery function
+  dryGrocery() {
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        param: "dryGrocery",
+        type: "dryGrocery"
+      }
+    };
+    this.router.navigate(['/product'], navigationExtras).then(() => {
+      window.location.reload();
+    });
+  }
+
+  //Cigarette function
+  cigarette(){
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        param: "cigarettes",
+        type: "cigarettes"
+      }
+    };
+    this.router.navigate(['/product'], navigationExtras).then(() => {
+      window.location.reload();
+    });
+  }
+
+  //despicableVape function
+  despicableVape(){
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        param: "despicableVape",
+        type: "despicableVape"
+      }
+    };
+    this.router.navigate(['/product'], navigationExtras).then(() => {
+      window.location.reload();
+    });
+  }
+
+  //electronic function
+  electronic(){
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        param: "electronic",
+        type: "electronic"
+      }
+    };
+    this.router.navigate(['/product'], navigationExtras).then(() => {
+      window.location.reload();
+    });
+  }
+
+  //health&Beauty function
+  healthAndBeauty(){
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        param: "healthAndBeauty",
+        type: "healthAndBeauty"
+      }
+    };
+    this.router.navigate(['/product'], navigationExtras).then(() => {
+      window.location.reload();
+    });
+  }
+
+  //produce function
+  produce(){
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        param: "produce",
+        type: "produce"
+      }
+    };
+    this.router.navigate(['/product'], navigationExtras).then(() => {
+      window.location.reload();
+    });
+  }
+
+  //tobacco function
+  tobacco(){
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        param: "tobacco",
+        type: "tobacco"
+      }
+    };
+    this.router.navigate(['/product'], navigationExtras).then(() => {
+      window.location.reload();
+    });
+  }
+
+  wareHouse(){
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        param: "wareHouse",
+        type: "wareHouse"
+      }
+    };
+    this.router.navigate(['/product'], navigationExtras).then(() => {
+      window.location.reload();
+    });
+  }
+
+  accessories(){
+    this.router.navigate(['']);
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        param: "accessories",
+        type: "accessories"
+      }
+    };
+    this.router.navigate(['/product'], navigationExtras).then(() => {
+      window.location.reload();
+    });
+  }
+
+  household(){
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        param: "household",
+        type: "household"
+      }
+    };
+    this.router.navigate(['/product'], navigationExtras).then(() => {
+      window.location.reload();
+    });
   }
 }
